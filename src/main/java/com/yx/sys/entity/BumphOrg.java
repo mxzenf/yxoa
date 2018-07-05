a883beac4dee7d6b07ce5db6ecb96029c5f6bc29
mxzenf <93839187@qq.com>
2018-07-05 13:31:05 +0800

    新增公文通知部分bean

---
+++ src/main/java/com/yx/sys/entity/BumphOrg.java
@@ -0,0 +1,53 @@
+package com.yx.sys.entity;
+
+/**
+ * Created by 杨欣 on 2018/7/4.
+ */
+public class BumphOrg {
+
+    private String id;
+    private String orgid;
+    private String orgname;
+    private String bumph_id;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrgid() {
+        return orgid;
+    }
+
+    public void setOrgid(String orgid) {
+        this.orgid = orgid;
+    }
+
+    public String getOrgname() {
+        return orgname;
+    }
+
+    public void setOrgname(String orgname) {
+        this.orgname = orgname;
+    }
+
+    public String getBumph_id() {
+        return bumph_id;
+    }
+
+    public void setBumph_id(String bumph_id) {
+        this.bumph_id = bumph_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
